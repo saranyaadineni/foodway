1e516d482f8de4583ee5930f10795f86e6eb95b6
saranyaadineni <asaranya@speshway.com>
2026-03-31 15:47:39 +0530

    fixes

--- FOODY-main/backend/controllers/order.controllers.js
+++ FOODY-main/backend/controllers/order.controllers.js
@@ -1,4 +1,4 @@
-import DeliveryAssignment from "../models/deliveryassignment.model.js"
+import DeliveryAssignment from "../models/deliveryAssignment.model.js"
 import Order from "../models/order.model.js"
 import Shop from "../models/shop.model.js"
 import User from "../models/user.model.js"
